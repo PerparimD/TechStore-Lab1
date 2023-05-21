be4d566589eeb08e5dfdab676b5ae61f38891ff2
Perparim <perparimdd@gmail.com>
2023-05-22 00:10:20 +0200

    Rregullime tek Log In dhe Sign up

--- techstore/src/Pages/LogIn.js
+++ techstore/src/Pages/LogIn.js
@@ -3,28 +3,35 @@ import { Helmet } from "react-helmet";
 import NavBar from "../Components/layout/NavBar";
 import Footer from "../Components/layout/Footer";
 import Form from "react-bootstrap/Form";
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
+import FloatingLabel from "react-bootstrap/FloatingLabel";
 import Button from "react-bootstrap/esm/Button";
 import "./Styles/LogIn.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelopeCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import Mesazhi from "../Components/layout/Mesazhi";
 import jwt_decode from "jwt-decode";
-
-
-
-
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBCard,
+  MDBCardBody,
+  MDBInput,
+  MDBIcon,
+  MDBCheckbox,
+} from "mdb-react-ui-kit";
+import { Link } from "react-router-dom";
 
 const LogIn = () => {
-
   const navigate = useNavigate();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loggedIn, setLoggedIn] = useState(false)
+  const [loggedIn, setLoggedIn] = useState(false);
 
   function vendosEmail(value) {
     setEmail(value);
@@ -35,33 +42,32 @@ const LogIn = () => {
   }
 
   useEffect(() => {
-    const loggedUser = localStorage.getItem('user')
+    const loggedUser = localStorage.getItem("user");
 
     if (loggedUser) {
-      setLoggedIn(true)
+      setLoggedIn(true);
     }
-  })
+  });
 
   const handleLogIn = (e) => {
     e.preventDefault();
 
-    fetch('https://localhost:7285/api/Authenticate/login', {
-      method: 'POST',
+    fetch("https://localhost:7285/api/Authenticate/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         email: email,
-        password: password
-      })
+        password: password,
+      }),
     })
-      .then(async response => {
+      .then(async (response) => {
         if (response.ok) {
-
           const data = await response.json();
           const user = {
-            email: email
-          }
+            email: email,
+          };
 
           if (data.token) {
             localStorage.setItem("user", JSON.stringify(data));
@@ -70,7 +76,7 @@ const LogIn = () => {
             localStorage.setItem("userInfo", JSON.stringify(user));
           }
 
-          console.log('Token:', data.token);
+          console.log("Token:", data.token);
           // console.log('Expiration:', data.expiration);
           const token = data.token;
 
@@ -78,66 +84,95 @@ const LogIn = () => {
           const decodedToken = jwt_decode(token);
 
           // Access the roles claim
-          const roles = decodedToken["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/email"];
-          console.log(roles)
+          const roles =
+            decodedToken[
+              "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/email"
+            ];
+          console.log(roles);
 
-          if (roles == 'Admin') {
+          if (roles == "Admin") {
             navigate("/dashboard");
-          } else if (roles == 'Manager') {
-            navigate('/dashboard')
-          } else if (roles == 'User') {
-            navigate('/')
+          } else if (roles == "Manager") {
+            navigate("/dashboard");
+          } else if (roles == "User") {
+            navigate("/");
           }
         } else {
-          throw new Error('Authentication failed');
+          throw new Error("Authentication failed");
         }
       })
-      .catch(error => {
-        console.error('Error:', error);
+      .catch((error) => {
+        console.error("Error:", error);
       });
-  }
+  };
 
-
-  return(
+  return (
     <div className="logIn">
-    <Helmet>
-      <title>Log In | Tech Store</title>
-    </Helmet>
-    <NavBar />
-    <Form className="login-form">
-          <Form.Text className="formTitle">Log In</Form.Text>
-          <FloatingLabel
+      <Helmet>
+        <title>Log In | Tech Store</title>
+      </Helmet>
+      <NavBar />
+      <MDBContainer fluid >
+        <MDBRow className="d-flex justify-content-center align-items-center h-100">
+          <MDBCol col="12">
+            <MDBCard
+              className="bg-white my-5 mx-auto"
+              style={{border: "none",boxShadow: "0 0 20px #ddd", borderRadius: "2rem", maxWidth: "500px" }}
+            >
+              <MDBCardBody className="p-5 w-100 d-flex flex-column">
+              <Form.Text className="formTitle">Log In</Form.Text>
+                <p className="text-white-20 mb-4 p-text">
+                  Please enter your email and password!
+                </p>
 
-            controlId="floatingInput"
-            label="Email"
-            className="mb-3 label"
-          >
-            <Form.Control className="input"
-              placeholder="Email"
-              type="text"
-             />
-          </FloatingLabel>
-          <FloatingLabel
+                <MDBInput
+                  wrapperClass="mb-4 w-100"
+                  label="Email address"
+                  id="formControlLg"
+                  type="email"
+                  size="lg"
+                />
+                <MDBInput
+                  wrapperClass="mb-4 w-100"
+                  label="Password"
+                  id="formControlLg"
+                  type="password"
+                  size="lg"
+                />
+                <Link to="/SignUp" className="text-white-20 mb-4 p-text">Don't have an Account?</Link>
+                <MDBBtn size="lg">Login</MDBBtn>
 
-            controlId="floatingInput"
-            label="Password"
-            className="mb-3 label"
-          >
-            <Form.Control className="input"
-              placeholder="Password"
-              type="text"
-             />
-          </FloatingLabel>
-          <button className="login-button">
-            Log In
-          </button>
-          
+                
+              </MDBCardBody>
+            </MDBCard>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
 
-    </Form>  
-    <Footer/>    
-  </div>
+      <Footer />
+    </div>
   );
-
 };
 
 export default LogIn;
+
+{
+  /* <Form className="login-form">
+        <Form.Text className="formTitle">Log In</Form.Text>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Email"
+          className="mb-3 label"
+        >
+          <Form.Control className="input" placeholder="Email" type="text" />
+        </FloatingLabel>
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Password"
+          className="mb-3 label"
+        >
+          <Form.Control className="input" placeholder="Password" type="text" />
+        </FloatingLabel>
+        <button className="login-button">Log In</button>
+      </Form> */
+}
--- techstore/src/Pages/SignUp.js
+++ techstore/src/Pages/SignUp.js
@@ -11,6 +11,8 @@ import { faEnvelopeCircleCheck } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import axios from "axios";
 import Mesazhi from "../Components/layout/Mesazhi";
+import Col from "react-bootstrap/Col";
+import { Row } from "react-bootstrap";
 
 const SignUp = () => {
   const [emri, setEmri] = useState("");
@@ -38,9 +40,6 @@ const SignUp = () => {
       })
       .then((response) => {
         console.log(response);
-        setEmri("");
-        setEmail("");
-        setPassword("");
       })
       .catch((error) => {
         console.log("error");
@@ -54,18 +53,124 @@ const SignUp = () => {
       </Helmet>
       <NavBar />
       <Form className="sign-up-form">
+      <Form.Text className="formTitle">Sign Up</Form.Text>
+
+        <Row className="mb-3">
+          <Form.Group as={Col}  className="p-0" controlId="formGridName">
+            <Form.Label>Name</Form.Label>
+            <Form.Control type="name" placeholder="Enter Name" />
+          </Form.Group>
+
+          <Form.Group as={Col} className="p-0" controlId="formGridLastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="last name" placeholder="Last Name" />
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="formGridAddress2">
+          <Form.Label>Username</Form.Label>
+          <Form.Control placeholder="Username" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formGridAddress2">
+          <Form.Label>Email</Form.Label>
+          <Form.Control placeholder="example@hotmail.com" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formGridAddress2">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formGridAddress2">
+          <Form.Label>Phone Number</Form.Label>
+          <Form.Control placeholder="045-000-000" />
+        </Form.Group>
+
+        <Row className="mb-3">
+          <Form.Group as={Col} className="p-0" controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control />
+          </Form.Group>
+
+          <Form.Group as={Col} className="p-0" controlId="formGridState">
+            <Form.Label>State</Form.Label>
+            <Form.Select defaultValue="Choose...">
+              <option>Choose...</option>
+              <option>...</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group as={Col} className="p-0" controlId="formGridZip">
+            <Form.Label>Zip</Form.Label>
+            <Form.Control />
+          </Form.Group>
+        </Row>
+        <Button variant="primary" type="submit" onClick={CreateAcc}>
+        Create Account
+        </Button>
+      </Form>
+      {/* <Form className="sign-up-form">
         <Form.Text className="formTitle">Sign Up</Form.Text>
+
+        <Row>
+          <Col>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Name"
+              className="mb-3 label"
+            >
+              <Form.Control
+                value={emri}
+                className="input"
+                placeholder=" Name"
+                type="text"
+                onChange={(e) => vendosEmrin(e.target.value)}
+              />
+            </FloatingLabel>
+          </Col>
+
+          <Col>
+            <FloatingLabel
+              controlId="floatingInput"
+              label="Last Name"
+              className="mb-3 label "
+            >
+              <Form.Control
+                className="input"
+                placeholder="Last Name"
+                type="text"
+              />
+            </FloatingLabel>
+          </Col>
+        </Row>
+
         <FloatingLabel
           controlId="floatingInput"
-          label="Name"
-          className="mb-3 label"
+          label="User Name"
+          className="mb-3 label "
         >
-          <Form.Control 
-          value={emri}
-          className="input" 
-          placeholder=" Name" 
-          type="text"
-          onChange={(e) => vendosEmrin(e.target.value)} />
+          <Form.Control className="input" placeholder="User Name" type="text" />
+        </FloatingLabel>
+
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Phone Number"
+          className="mb-3 label "
+        >
+          <Form.Control
+            className="input"
+            placeholder="Phone Number"
+            type="text"
+          />
+        </FloatingLabel>
+
+        <FloatingLabel
+          controlId="floatingInput"
+          label="Adress"
+          className="mb-3 label "
+        >
+          <Form.Control className="input" placeholder="Adress" type="text" />
         </FloatingLabel>
 
         <FloatingLabel
@@ -74,7 +179,7 @@ const SignUp = () => {
           className="mb-3 label"
         >
           <Form.Control
-          value={email}
+            value={email}
             className="input"
             placeholder=" exmple@email.com"
             type="email"
@@ -87,15 +192,18 @@ const SignUp = () => {
           label="Password"
           className="mb-3 label"
         >
-          <Form.Control 
-          value={password}
-          className="input" 
-          placeholder="Password" 
-          type="text" 
-          onChange={(e) => vendosPasswordin(e.target.value)}/>
+          <Form.Control
+            value={password}
+            className="input"
+            placeholder="Password"
+            type="text"
+            onChange={(e) => vendosPasswordin(e.target.value)}
+          />
         </FloatingLabel>
-        <button className="sign-up-button" onClick={CreateAcc}>Create Account</button>
-      </Form>
+        <button className="sign-up-button" onClick={CreateAcc}>
+          Create Account
+        </button>
+      </Form> */}
       <Footer />
     </div>
   );

529b0b7008a38bdb083a269fc937bb395e7c6977
Rilind Kyçyku <r.kycyku.12@gmail.com>
2024-07-03 10:27:54 +0200

    Eshte larguar pjesa e Demos
    Eshte larguar pjesa e Demos pershkak te mosfunksionalitetit

--- Demo/techstore/src/Components/layout/Buton.js
+++
@@ -1,9 +0,0 @@
-import classes from './Styles/Buton.module.css';
-
-function Buton(props) {
-    return (
-        <button  type="submit" onClick={props.onClick} className={classes.buttonat} name={props.Name}>{props.Label}</button>
-    );
-}
-
-export default Buton;
